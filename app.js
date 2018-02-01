7906cc9a87eb8ae1ea9a0b4ffb4ace29f260c998
Keli Hansen <keli.p.hansen@gmail.com>
2018-02-01 15:45:03 -0800

    worked on loop functionality in preparation in creating function

--- app.js
+++ app.js
@@ -18,15 +18,17 @@ const animal = prompt('And last but not least, what is your favorite animal?');
 window.alert ('That is so interesting, that your favorite animal is ' + animal);
 console.log('Your favorite animal is ' + animal);
 
-// ice cream game - need to figure out how to make responses other than y and n say something other than the no response.
+ice cream game - need to figure out how to make responses other than y and n say something other than the no response.
 
 alert ('Now let\'s play a game!!!!');
 
+const yourFlavors = [];
 const likesChocolate = prompt('Do you like chocolate icecream?');
 
 if (likesChocolate.toLowerCase() === 'yes' || likesChocolate.toLowerCase() === 'y') {
     alert('That\'s so funny, chocolate is actually good for you! In moderation that is..');
     console.log('User likes chocolate icecream');
+    yourFlavors.push('Chocolate');
 }   else {(likesChocolate.toLowerCase() === 'no' || likesChocolate.toLowerCase() === 'n');
     alert('What?!?! Why not?? Chocolate is good for you dude/dudette!');
     console.log('User does not like chocolate icecream');}
@@ -36,6 +38,7 @@ const likesVanilla = prompt('Do you like Vanilla icecream??');
 if (likesVanilla.toLowerCase() === 'yes' || likesVanilla.toLowerCase() === 'y') {
     alert('Yay!!! For Vanilla icecream!');
     console.log('User likes vanilla icecream');
+    yourFlavors.push('Vanilla');
 }   else {(likesVanilla.toLowerCase() === likesVanilla.toLowerCase() === 'no' || 'n');
     alert('Pffff!!!!! Whatever!');
     console.log('User does not like vanilla icecream');}
@@ -45,6 +48,7 @@ const likesPistacio = prompt('Do you like Pistacio icecream?');
 if (likesPistacio.toLowerCase() === 'yes' || likesPistacio.toLowerCase() === 'y') {
     alert('Heck yes! Pistacio iceream rocks!');
     console.log('User likes pistacio icecream');
+    yourFlavors.push('Pistacio');
 }   else {(likesPistacio.toLowerCase() === 'no' || likesPistacio.toLowerCase() === 'n');
     alert('Pistacio icecream is the best though!?!');
     console.log('User does not like pistacio icecream');}
@@ -54,6 +58,7 @@ const likesStrawberry = prompt ('Do you like Strawberry icecream?');
 if (likesStrawberry.toLowerCase() === 'yes' || likesStrawberry.toLowerCase() === 'y') {
     alert('Strawberry fields forever!!!');
     console.log('User likes strawberry icecream');
+    yourFlavors.push('Strawberry');
 }   else {(likesStrawberry.toLowerCase() === 'no' || likesStrawberry.toLowerCase() === 'n');
     alert('Well, they are high in vitamin C, so maybe you should START LIKING THEM!');
     console.log('User does not like strawberry icecream');}
@@ -63,39 +68,43 @@ const likesCoffee = prompt('Do you like Coffee icecream?');
 if (likesCoffee.toLowerCase() === 'yes' || likesCoffee.toLowerCase() === 'y') {
     alert('What\'s better than drinking coffee??? Eating it!');
     console.log('User likes coffee icecream');
+    yourFlavors.push('Coffee');
 }   else {(likesCoffee.toLowerCase() === 'no' || likesCoffee.toLowerCase() === 'n');
     alert('Dude/Dudette...really? Just really?');
     console.log('User does not like coffee icecream');}
 
 // for loop program
 
-const yourFlavors = [];
+let guess;
 
-for (let i = 0; i < 31; i ++) {
-    const guess = parseInt(prompt('Do you know how many flavors of icecream we have?'));
+for (let i = 0; i < 31; i++) {
+    guess = parseInt(prompt('Do you know how many flavors of icecream we have?'));
     if (guess === 0) {
         alert('We definitely have more than that!');
-    } else if (guess < 10) {
+    } else if (guess > 0 && guess <= 10) {
         alert('Keep trying! We have more than ten!');
-    } else if (guess < 15) {
+    } else if (guess > 10 && guess <= 15) {
         alert('Keep going, hint think about a famous icecream chain');
-    } else if (guess < 20) {
+    } else if (guess > 15 && guess <= 20) {
         alert('Almost!!!');
-    } else if (guess < 31) {
+    } else if (guess > 20 && guess < 31) {
         alert('Just about there!!!');
+    } else if (guess > 31) {
+        alert('Too much!');
     } else if (guess === 31) {
-        alert('Good job!');
-        yourFlavors.push(); // need to figure out how to store user y and n's
-        // into const yourFlavors, so it can print what icecream they should order in the end.
         break;
     }
 }
-alert('You should order these flavors when you come into the shop ' + yourFlavors); // need to fix
+
+if (guess === 31) {
+    alert('Good job!');
+}
+alert('You should order these flavors when you come into the shop: ' + yourFlavors.join(', ') + '!'); // need to fix
 
 // new while loop program
 
 const myNumber = 27;
-const userNumber = prompt('Guess a number');
+let userNumber = prompt('Guess a number');
 console.log('To exit the loop type in 27'); // added exit number
 
 while (userNumber != myNumber) {
